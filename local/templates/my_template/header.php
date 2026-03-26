e147b291fc0309e1cf226c7d063bb1acf2bb148b
Yoshikobaru <hardpush123@mail.ru>
2026-03-26 19:04:31 +0700

    pan

--- local/templates/my_template/header.php
+++ local/templates/my_template/header.php
@@ -6,6 +6,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?=$APPLICATION->GetTitle()?></title>
+	<?$APPLICATION->ShowHead();?>
 	<link rel="stylesheet" href="<?=SITE_TEMPLATE_PATH?>/assets/css/swiper-bundle.min.css">
 	<link rel="stylesheet" href="<?=SITE_TEMPLATE_PATH?>/assets/css/fancybox.css">
 	<link rel="stylesheet" href="<?=SITE_TEMPLATE_PATH?>/assets/css/main.css">
