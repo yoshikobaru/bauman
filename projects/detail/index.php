d250583acc068ac9b20a93ccec55e57b458fddf9
Yoshikobaru <hardpush123@mail.ru>
2026-05-14 02:26:16 +0700

    fdsfds

--- projects/detail/index.php
+++ projects/detail/index.php
@@ -119,7 +119,7 @@ if (!empty($arElement['DETAIL_PICTURE'])) {
         <div class="container">
             <h2 class="main-title project-help__title">Проекту необходима финансовая поддержка</h2>
             <p class="project-help__text main-text">
-                Мы рады любой помощи вне зависимости от её размера. ля компаний желающих стать спонсорами либо участниками данного проекта - просим связаться с нами.
+                Мы рады любой помощи вне зависимости от её размера. Для компаний желающих стать спонсорами либо участниками данного проекта - просим связаться с нами.
 </p>
             <button class="btn project-help__btn" data-fancybox data-src="#form-finance-help">Связаться с организаторами</button>
         </div>
